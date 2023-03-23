dc569852cc2ec98e45dd419106cc9123ceb0d88f
Adam Ranson <ranson.ad@gmail.com>
2023-03-23 15:35:37 +0100

    misc

--- run_step1_batch.py
+++ run_step1_batch.py
@@ -1,10 +1,10 @@
-import sys
-sys.path.append('~/code/preprocess_py')
-import preprocess_py
-expIDs = ['2023-03-01_01_ESMT107','2023-03-01_02_ESMT107']
+import preprocess_step1
+
+expIDs = ['2023-03-01_01_ESMT107']
 userID = 'adamranson'
 suite2p_config = 'ch_1_depth_1.npy'
 
 for expID in expIDs:
     print('Starting expID...')
-    preprocess_step1.run_preprocess_step1(userID,expID,suite2p_config)
+    # the last "True, True" determined if you run suite2p and dlc respectively
+    preprocess_step1.run_preprocess_step1(userID,expID,suite2p_config,False,False,True) 
---
+++ run_step2_batch.py
@@ -0,0 +1,10 @@
+import preprocess_step2
+
+expIDs = ['2023-03-01_01_ESMT107']
+userID = 'adamranson'
+suite2p_config = 'ch_1_depth_1.npy'
+
+for expID in expIDs:
+    print('Starting expID...' + expID)
+    # final ops are presecs, post secs and whether to process: 1.bonvision, 2.s2p_timestamp, 3.ephys, 4.dlc_timestamp, 5.cutraces
+    preprocess_step2.run_preprocess_step2(userID,expID, 5, 5, True,False,True,True,True)
