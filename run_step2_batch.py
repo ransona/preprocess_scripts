1c6c2ecc58a370e21c8e42f702800ff261fb4063
Adam Ranson <ranson.ad@gmail.com>
2023-04-05 20:17:09 +0200

    see previous

--- run_step2_batch.py
+++ run_step2_batch.py
@@ -16,10 +16,10 @@ run_ephys = True
 run_dlc_timestamp = True
 run_cuttraces = True
 
-# for expID in expIDs:
-#     print('Starting expID...' + expID)
-#     # final ops are presecs, post secs and whether to process: 1.bonvision, 2.s2p_timestamp, 3.ephys, 4.dlc_timestamp, 5.cutraces
-#     preprocess_step2.run_preprocess_step2(userID,expID, pre_secs, post_secs, run_bonvision, run_s2p_timestamp, run_ephys, run_dlc_timestamp, run_cuttraces)
+for expID in expIDs:
+    print('Starting expID...' + expID)
+    # final ops are presecs, post secs and whether to process: 1.bonvision, 2.s2p_timestamp, 3.ephys, 4.dlc_timestamp, 5.cutraces
+    preprocess_step2.run_preprocess_step2(userID,expID, pre_secs, post_secs, run_bonvision, run_s2p_timestamp, run_ephys, run_dlc_timestamp, run_cuttraces)
 #     # set permissions all files generated to user
 #     try:
 #         animalID, remote_repository_root, processed_root, exp_dir_processed, exp_dir_raw = organise_paths.find_paths(userID, expID)
