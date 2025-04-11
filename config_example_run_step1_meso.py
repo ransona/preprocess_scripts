7ffe9ca12f911f0b1827a065b9b87abf84f58463
Adam Ranson <7849066+ransona@users.noreply.github.com>
2025-04-11 15:36:38 +0200

    added configs to synced part of repo

---
+++ config_example_run_step1_meso.py
@@ -0,0 +1,23 @@
+# example config file for running step 1.
+# this file should be saved in the configs folder
+
+import getpass
+from run_step1_batch_meso import run_step1_batch_meso
+
+step1_config = {}
+username = getpass.getuser()
+step1_config['userID'] = 'adamranson' #username # defines where processed data will be stored and subsequently sought
+step1_config['expIDs'] = ['2025-04-10_26_TEST']
+# suite2p config will potentially be specific to scan path and also to the roi
+# the nested list is for the two scan paths, and witin each scan path, the lists are for the multiple ROIs
+# if within the scan path there is only one suite2p config, then this is applied to all rois in that scan path
+step1_config['suite2p_config'] = [['ch_1_depth_1_artifact.npy'],['ch_1_depth_1_artifact.npy']]
+# step1_config['suite2p_config'] = 'ch_1_depth_9_artifact.npy'
+#  step1_config['suite2p_config'] = 'ch_1_2_depth_5_artifact.npy'
+step1_config['runs2p'] = True 
+step1_config['rundlc'] = True
+step1_config['runfitpupil'] = True
+step1_config['jump_queue'] = False
+
+# run this config
+run_step1_batch_meso(step1_config)
--- config_example_run_step2.py
+++ config_example_run_step2.py
@@ -6,7 +6,7 @@ from run_step2_batch import run_step2_batch
 step2_config = {}
 
 step2_config['userID'] = 'adamranson'
-step2_config['expIDs'] = ['2023-10-31_03_ESMT151'] 
+step2_config['expIDs'] = ['2025-04-10_26_TEST'] 
 step2_config['pre_secs'] = 5
 step2_config['post_secs'] = 5
 step2_config['run_bonvision'] = True
@@ -17,7 +17,8 @@ step2_config['run_cuttraces'] = True
 
 # other setting that can be used in the pipeline if needed by loading the step2 settings file 
 settings = {}
-settings['neuropil_coeff'] = [0,0.7] # one for each channel, defaults to 0.7 if not set
+settings['neuropil_coeff'] = [0.7,0.7] # one for each channel, defaults to 0.7 if not set
+settings['neuropil_coeff'] = [0,0] # one for each channel, defaults to 0.7 if not set
 settings['subtract_overall_frame'] = False
 step2_config['settings'] = settings
 
